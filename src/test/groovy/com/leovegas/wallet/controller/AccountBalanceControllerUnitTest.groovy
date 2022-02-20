78e7113f22eca8d3782276fa86beaf981047641e
Konrad <konrad.poczatek@stanusch.com>
2022-02-20 11:16:35 +0100

    add unit tests of account balance controller

--- src/test/groovy/com/leovegas/wallet/controller/AccountBalanceControllerUnitTest.groovy
+++ src/test/groovy/com/leovegas/wallet/controller/AccountBalanceControllerUnitTest.groovy
@@ -1,5 +1,7 @@
 package com.leovegas.wallet.controller
 
+import com.leovegas.wallet.exception.PlayerNotFoundException
+import com.leovegas.wallet.model.Player
 import com.leovegas.wallet.repository.PlayerRepository
 import spock.lang.Specification
 
@@ -7,8 +9,21 @@ class AccountBalanceControllerUnitTest extends Specification {
     def playerRepository = Mock(PlayerRepository)
     def subject = new AccountBalanceController(playerRepository)
 
-    def "getBalance return my balance"() {
+    def "getBalance return balance of player"() {
+        given:
+            def player = new Player(id: 1, balance: 100)
+            playerRepository.findById(1) >> Optional.of(player)
         expect:
-            subject.getBalance() == 'My balance'
+            subject.getBalance(1) == 100
+    }
+
+    def "getBalance when player does not exist then throw player not found exception"() {
+        given:
+            playerRepository.findById(1) >> Optional.empty()
+        when:
+            subject.getBalance(1)
+        then:
+            def e = thrown(PlayerNotFoundException)
+            e.message == 'Player with given id does not exist'
     }
 }
